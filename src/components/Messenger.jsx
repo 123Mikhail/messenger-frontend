57071f3e4121f4e5a88ea2be0465b289b3b9d469
User <mikhailbogdan2006@gmail.com>
2026-05-15 03:08:18 +0300

    fix websocket https protocol

--- src/components/Messenger.jsx
+++ src/components/Messenger.jsx
@@ -86,7 +86,7 @@ export default function Messenger() {
   };
 
   useEffect(() => {
-    const socket = new SockJS('http://localhost:8080/ws');
+    const socket = new SockJS('https://messenger-xmx7.onrender.com/ws');
     const client = new Client({
       webSocketFactory: () => socket,
       onConnect: () => setStompClient(client),
